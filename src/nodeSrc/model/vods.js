2894babdb910dc79062d69eec34554b81351d576
Blanksoon <farm1771@gmail.com>
2017-09-14 17:53:58 +0700

    test

--- src/nodeSrc/model/vods.js
+++ src/nodeSrc/model/vods.js
@@ -1,25 +1,25 @@
 // grab the things we need
-var mongoose = require('mongoose');
+var mongoose = require('mongoose')
 var options = {
-  useMongoClient: true
-};
+  useMongoClient: true,
+}
 
-mongoose.connect('mongodb://mongo/max_api', options);
+mongoose.connect('mongodb://mongo/max_api', options)
 
-var Schema = mongoose.Schema;
+var Schema = mongoose.Schema
 
 // create a schema
 var vodSchema = new Schema({
   title: { type: String, required: true, unique: true },
-  duration : Number,
+  duration: Number,
   on_air_date: Date,
-  video_url : String,
-  long_url : String
-});
+  video_url: String,
+  long_url: String,
+})
 
 // the schema is useless so far
 // we need to create a model using it
-var Vods = mongoose.model('vods', vodSchema);
+var Vods = mongoose.model('vods', vodSchema)
 
 // make this available to our users in our Node applications
-module.exports = Vods;
+module.exports = Vods
