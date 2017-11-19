a4e1bc025bf57f57d69f38b58fd06bfdded4c997
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2017-11-19 14:26:46 +0700

    refactor

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 const env = require('./src/config/env')
-app = require('express')()
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+// Global import
 mongoose = require('mongoose')
-cors = require('cors')
 Vod = require('./src/models/vod')
 User = require('./src/models/user')
 Order = require('./src/models/order')
 Live = require('./src/models/live')
 Subscribe = require('./src/models/subscribe')
-bodyParser = require('body-parser')
-express = require('express')
-//'mongodb://mmt:maxworldchampion2016@localhost/max_api_phase2',
-//'mongodb://mmt:maxworldchampion2016@beta.maxmuaythai.com/max_api_phase2',
+
+// Config depedencies
 mongoose.connect(env.MONGO_CONNS, {
   useMongoClient: true,
 })
+
+// Application specific variables
+app.set('secret', env.JWT_SECRET)
+app.set('tokenLifetime', env.JWT_TOKEN_LIFETIME)
+
+// Middleware
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
-app.set('secret', env.JWT_SECRET)
-app.set('tokenLifetime', env.JWT_TOKEN_LIFETIME)
 app.use(express.static('./public'))
 
+// Config routes
 var routes = require('./src/config/route')
 routes(app)
 
