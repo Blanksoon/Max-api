0aab402cbcaceae6067af793653fad4f09d74c03
Blanksoon <farm1771@gmail.com>
2017-12-19 15:43:28 +0700

    test webhook

--- src/controllers/ppcheckoutController.js
+++ src/controllers/ppcheckoutController.js
@@ -581,7 +581,9 @@ exports.webhookHandler = async function(req, res) {
 }
 exports.stripeWebhookHandler = async function(req, res) {
   const payload = req.body
-  console.log(req.body)
+  console.log('body.......', req.body)
+  console.log('params......', req.params)
+  console.log('query......', req.query)
   fs.writeFileSync('./stripewebhook.txt', JSON.stringify(payload), {
     flag: 'a',
   })
