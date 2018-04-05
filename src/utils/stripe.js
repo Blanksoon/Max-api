f92a7fb5a8993a7032784bb75fd173eb324010e4
Blanksoon <farm1771@gmail.com>
2018-04-05 17:16:19 +0700

    test stripe

--- src/utils/stripe.js
+++ src/utils/stripe.js
@@ -204,35 +204,35 @@ export function createNeworderSubscribe(paymentId) {
       .then(async function(orderData) {
         const today = Date.now()
         console.log('orderData: ', orderData)
-        if (orderData.expiredDate >= today || orderData.expiredDate === null) {
-          resolve(orderData)
-        } else {
-          const data = orderData.toObject()
-          const orders = _.omit(data, ['_id'])
-          const oldOrder = orderData
-          oldOrder.status = 'expired'
-          await oldOrder.save() //old order
-          let expiredDate = moment(today) // new order
-            .add(1, 'M')
-            .calendar()
-          const newOrder = new Order({
-            productId: orders.productId,
-            productName: orders.productName,
-            userId: orders.userId,
-            email: orders.email,
-            price: orders.price,
-            purchaseDate: new Date(),
-            platform: orders.platform,
-            expiredDate: expiredDate,
-            status: 'approved',
-            stripe: orders.stripe,
-            cancelDate: null,
-            orderId: orders.orderId,
-            paymentIos: orders.paymetIos,
-          })
-          await newOrder.save()
-          resolve(newOrder)
-        }
+        // if (orderData.expiredDate >= today || orderData.expiredDate === null) {
+        //   resolve(orderData)
+        // } else {
+        const data = orderData.toObject()
+        const orders = _.omit(data, ['_id'])
+        const oldOrder = orderData
+        oldOrder.status = 'expired'
+        await oldOrder.save() //old order
+        let expiredDate = moment(today) // new order
+          .add(1, 'M')
+          .calendar()
+        const newOrder = new Order({
+          productId: orders.productId,
+          productName: orders.productName,
+          userId: orders.userId,
+          email: orders.email,
+          price: orders.price,
+          purchaseDate: new Date(),
+          platform: orders.platform,
+          expiredDate: expiredDate,
+          status: 'approved',
+          stripe: orders.stripe,
+          cancelDate: null,
+          orderId: orders.orderId,
+          paymentIos: orders.paymetIos,
+        })
+        await newOrder.save()
+        resolve(newOrder)
+        //}
       })
       .catch(function(err) {
         console.log('err', err)
